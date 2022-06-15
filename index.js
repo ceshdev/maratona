bfe734131af8f9e2962e7f410d7b9267506a7178
ceshdev <106279767+ceshdev@users.noreply.github.com>
2022-06-15 15:13:48 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+const elementoResposta = document.querySelector('#resposta')
+const inputPergunta = document.querySelector('#inputPergunta')
+const buttonPerguntar = document.querySelector('#buttonPerguntar')
+const respostas = [
+  'Certeza',
+  'Não tenho tanta certeza',
+  'É decididamente assim',
+  'Não conte com isso',
+  'Sem duvidas!',
+  'Pergunte novamente mais tarde',
+  'Sim, definitivamente!',
+  'Minha resposta é não.',
+  'Você pode contar com isso.',
+  'Melhor não te dizer agora.',
+  'A meu ver, sim.',
+  'Minhas fontes dizem não.',
+  'Provavelmente.',
+  'Não é possivel prever agora.',
+  'Perspectiva boa.',
+  'As perspectiva não são tão boas.',
+  'Sim.',
+  'Concentre-se e pergunte novamente.',
+  'Sinais apontam que sim.'
+]
+
+function fazerPergunta() {
+  if (inputPergunta.value == '') {
+    alert('digite sua pergunta')
+    return
+  }
+
+  buttonPerguntar.setAttribute('disabled', true)
+
+  const pergunta = '<div>' + inputPergunta.value + '</div>'
+
+  const totalRespostas = respostas.length
+  const numeroAleatorio = Math.floor(Math.random() * totalRespostas)
+
+  elementoResposta.innerHTML = pergunta + respostas[numeroAleatorio]
+
+  elementoResposta.style.opacity = 1
+
+  setTimeout(function () {
+    elementoResposta.style.opacity = 0
+    buttonPerguntar.removeAttribute('disabled')
+  }, 3000)
+}
